e5ac300db40de3c76315ec01866194c4f0e3e2d0
dipak560035 <dipaksah2070@gmail.com>
2025-12-10 09:19:32 +0545

    change

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -2,13 +2,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 //
 
-export const base = 'https://rtk-query-oc7w.onrender.com/';
+export const base = 'https://rtk-query-oc7w.onrender.com';
 
 export const mainApi = createApi({
     reducerPath:'mainApi',
-    baseQuery:fetchBaseQuery({baseUrl: 'https://rtk-query-oc7w.onrender.com/'}),
+    baseQuery:fetchBaseQuery({ baseUrl: 'https://rtk-query-oc7w.onrender.com/api' }),
     tagTypes: ['Products'],
-
     endpoints:() => ({})
 });
 
