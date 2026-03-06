8922fbcfb7f576c3a873bb77c73b7834e96fc599
dipak560035 <dipaksah2070@gmail.com>
2026-03-06 23:10:44 +0545

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
         <p>
           At HeavenCraft, we are committed to delivering premium-quality furniture and home solutions that embody craftsmanship, innovation, and timeless design. Our focus is on combining aesthetic excellence with functional durability, ensuring every product enhances both the beauty and comfort of modern living spaces. Guided by a customer-first philosophy, we strive to provide a seamless and trustworthy shopping experience through carefully curated collections, transparent service, and dependable delivery. HeavenCraft stands for quality you can rely on, design you can appreciate, and a standard of excellence that transforms houses into refined, inspiring homes.
 
-             At HeavenCraft, we are committed to delivering premium-quality furniture and home solutions that embody craftsmanship, innovation, and timeless design. Our focus is on combining aesthetic excellence with functional durability, ensuring every product enhances both the beauty and comfort of modern living spaces. Guided by a customer-first philosophy, we strive to provide a seamless and trustworthy shopping experience through carefully curated collections, transparent service, and dependable delivery. HeavenCraft stands for quality you can rely on, design you can appreciate, and a standard of excellence that transforms houses into refined, inspiring homes.
+          
         </p>
       </Container>
     </>
