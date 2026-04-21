7dbd94af7835cd717eb7c43210c3dcfffae427ed
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-04-21 22:12:07 +0545

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,7 @@
 
 
 
-// import * as React from "react"
+// import * as React from "reac
 // import { Slot } from "@radix-ui/react-slot
 // import { cva, type VariantProps } from "class-variance-authority
 // import { cn } from "@/lib/utils
