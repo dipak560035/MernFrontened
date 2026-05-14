238b4dfc9efdcd59edcbdd35c41d110eaf460ffe
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-05-14 21:10:48 +0545

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,18 @@ export default function Home() {
   const products =
     (Array.isArray(data?.data) && data.data) ||
     (Array.isArray(data) && data) ||
+import Container from "../components/layout/Container";
+import Button from "../components/ui/button";
+import { Link } from "react-router-dom";
+import { useProductsQuery } from "../services/api";
+import ProductCard from "../components/common/ProductCard";
 
+export default function Home() {
+  const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4001";
+
+  const { data, isLoading } = useProductsQuery({ limit: 4 });
+  const products =
+    (Array.isArray(data?.data) && data.data) |
 
 
 
